941e3f6c8937d1d4514dbf9771ea920aa9fe3842
German Koning <german@geko.com.ar>
2022-10-18 18:51:33 -0300

    Final Commit

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-let form = document.getElementById('Contacto');
-
-function sendForm(e) {
-    console.log('hola');
-    let data = {
-        name: document.getElementById('Nombre').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value,
-        phone: document.getElementById('phone').value
-    }
 
 
-    fetch('http://cantilever.com.ar:7777', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+function fullscreenImg (url) {
+    var img = document.createElement('img')
+    img.src = url
+    img.style.position = 'fixed'
+    img.style.top = 0
+    img.style.left = 0
+    img.style.width = '100%'
+    img.style.height = '100%'
+    img.style.zIndex = 9999
+    img.style.pointerEvents = 'none'
+    document.body.appendChild(img)
 }
 
 
-document.getElementById('sendbtn').addEventListener('click', sendForm());
+document.addEventListener('click', function (e) {
+    var target = e.target
+    if (target.tagName === 'IMG') {
+        Swal.fire({
+            html: `<img src=${target.src}>`,
+            showCloseButton: true,
+            showCancelButton: false,
+            showConfirmButton: false,
+            customClass: 'swal-wide',
+          })
+    }
+})
